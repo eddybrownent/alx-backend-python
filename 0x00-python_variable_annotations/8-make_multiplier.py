796c26c4aed832c5ab13f541c195557bb6eede45
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-07 23:17:16 +0300

    Complex types - functions Callable

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Creates a multiplier function
+
+    Args:
+        multiplier (float): rthe multiplier value
+
+    Returns:
+        Callable[[float], float]: a function
+    """
+    def multiplier_function(value: float) -> float:
+        return value * multiplier
+    
+    return multiplier_function
+
+
