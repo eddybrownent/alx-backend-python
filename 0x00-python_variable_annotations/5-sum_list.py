cd750646a09bff0f73bb70dcc37405341c31af2b
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-07 12:35:24 +0300

    adding documentation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,4 +6,11 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """
+    Args:
+        inpu_list : (List[float])
+
+    Returns:
+        float: sum of input_list
+    """
     return sum(input_list)
