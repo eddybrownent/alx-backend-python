d48b5748b87fd2780cdfe97c1fa92bb4fd1421a9
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-07 23:23:53 +0300

    fixing pycodestyle requirements

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -17,7 +17,5 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     def multiplier_function(value: float) -> float:
         return value * multiplier
-    
+
     return multiplier_function
-
-
