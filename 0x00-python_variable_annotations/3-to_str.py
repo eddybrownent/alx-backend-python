88c7f58d79a610f93e319241e2e8e5dc94893edf
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-07 12:17:22 +0300

    This is a function that takes a float and returns the string rep of it

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        n: (float)
+    Return:
+        n: (str) string representation of the float
+    """
+    return str(n)
