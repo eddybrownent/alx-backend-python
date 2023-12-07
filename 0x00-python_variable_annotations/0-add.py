b084727eca55f1273ce8d728a21dcf9ab07d74ac
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-07 12:12:31 +0300

    type-annotated function add that takes two floats as arguments and returns their sum

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floats
+
+    Args:
+        a (float)
+        b (float)
+
+    Returns:
+        float: sum of args
+    """
+    return a + b
