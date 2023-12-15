a31b2884e5c9de72911844f2d38448b9a3892e5a
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-15 21:36:07 +0300

    oroutine that will execute async_comprehension four times in parallel using asyncio.gather

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+measures execution time
+"""
+
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Coroutine measures runtime of executing async_comprehension 4 times
+
+    Returns:
+        float: Total runtime in secs
+    """
+    start_time = asyncio.get_event_loop().time()
+    task = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*task)
+    end_time = asyncio.get_event_loop().time()
+    return (end_time - start_time)
