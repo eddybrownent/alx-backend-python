92cf8b226cd46f4abc9f1c822eff19a3f50e56eb
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-15 20:20:17 +0300

    Synchronous generator function that yields random numbers between 0 and 10

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Asynchronous generator coroutine
+"""
+import random
+import asyncio
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    generates random nums btwn 0 & 10 after waiting for 1 sec
+
+    Results:
+        float: A random float between 0 and 10
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
