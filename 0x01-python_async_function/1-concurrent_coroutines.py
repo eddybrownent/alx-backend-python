94860e77722b1a3dc0acffb3d6843b0b6f47a5eb
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-15 13:05:44 +0300

    imported the List module for type annotations

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,10 +4,11 @@ Async spawns wait_random n times with a max_delay
 """
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: float = 10) -> list[float]:
+async def wait_n(n: int, max_delay: float = 10) -> List[float]:
     """
     Args:
         n (int): num of time to spawn wait_random.
