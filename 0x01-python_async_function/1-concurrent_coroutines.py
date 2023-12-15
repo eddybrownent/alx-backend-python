ca5b8ebb8477c2534b2036020f887e471c9f179c
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-15 12:19:02 +0300

    executing multiple coroutines at the same time with async

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Async spawns wait_random n times with a max_delay
+"""
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: float = 10) -> list[float]:
+    """
+    Args:
+        n (int): num of time to spawn wait_random.
+        max_delay (float, optional): max delay in secs Defaults 10
+
+    Returns:
+        list[float]: List of delays in ascending order
+    """
+    values = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    return sorted(values)
